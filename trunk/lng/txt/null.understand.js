989b58c6e636872758151939d6228184374922e4
eeddow <eeddow@556927e6-f9aa-11dd-9d1a-b59b2e1864b6>
2009-09-12 08:37:00 +0000

    Big spring refactoring - progression

--- trunk/lng/txt/null.understand.js
+++ trunk/lng/txt/null.understand.js
@@ -18,7 +18,7 @@ nul.understanding = {
 			return this.understand(ub);				
 		});
 		var operator = operator;
-		nul.possibles.map(ops, function(klg) {
+		nul.possibles.map(ub.klg, ops, function(klg) {
 			if(['+' ,'*'].contains(this.operator))
 				return new nul.obj.operation.Nary(operator, this);
 			if(['-' ,'/' ,'%'].contains(this.operator))
@@ -30,10 +30,10 @@ nul.understanding = {
 
 			switch(this.operator)
 			{
-				case ':-':	return new nul.obj.pair(this[0], this[1]);
+				case ':-':	return new nul.obj.pair(this[0], this[1], klg);
 				case ',':
 					var rv = this.follow?this.follow:new nul.obj.empty();
-					while(this.length) rv = new nul.obj.pair(this.pop(), rv);
+					while(this.length) rv = new nul.obj.pair(this.pop(), rv, klg);
 					return rv;
 				case '=': return klg.unify(this);
 				case ';':
@@ -45,14 +45,14 @@ nul.understanding = {
 	preceded: function(ub) {
 		var operand = this.operand.understand(ub);
 		var operator = this.operator;
-		return nul.possibles.map({op: operand}, function(klg) {
+		return nul.possibles.map(ub.klg, {op: operand}, function(klg) {
 			return new nul.obj.attribute(this.op, ' '+operator);
 		});
 	},
 	postceded: function(ub) {
 		var operand = this.operand.understand(ub);
 		var operator = this.operator;
-		return nul.possibles.map({op: operand}, function(klg) {
+		return nul.possibles.map(ub.klg, {op: operand}, function(klg) {
 			return new nul.obj.attribute(this.op, operator+' ');
 		});
 	},
@@ -81,17 +81,17 @@ nul.understanding = {
 	application: function(ub) {
 		var item = this.item.understand(ub);
 		var applied = this.applied.understand(ub);
-		return nul.possibles.map({itm: item, app: applied}, function(klg) {
-			var rv = this.itm.has(this.app);
+		return nul.possibles.map(ub.klg, {itm: item, app: applied}, function(klg) {
+			var rv = this.itm.has(this.app, klg);
 			if(rv) return rv;
-			klg.belong(this.itm, this.app);
+			klg.belong(this.app, this.itm);
 			return this.app;
 		});
 	},
 	taking: function(ub) {
 		var item = this.item.understand(ub);
 		var applied = this.applied.understand(ub);
-		return nul.possibles.map({itm: item, app: applied}, function() {
+		return nul.possibles.map(ub.klg, {itm: item, app: applied}, function() {
 			return this.app.through(this.itm);
 		});
 	},
@@ -102,7 +102,7 @@ nul.understanding = {
 		else cnt = [cnt];
 		
 		var slf = this.selfRef;
-		return [nul.possibles(cnt.mar(function() {
+		return [new nul.possibles(ub.klg, cnt.mar(function() {
 			//Understand each operands in a freshly created UB that stores locals
 			return new nul.understanding.base.set(ub, slf).understand(this);
 		})).set()];
@@ -121,14 +121,14 @@ nul.understanding = {
 	},
 
 	composed: function(ub) {
-		return nul.possibles([
+		return new nul.possibles(ub.klg, [
 			new nul.obj.byAttr(map(this.vals, function() { return this.understand(ub); }))
 		]);
 	},
 	objectivity: function(ub) {
 		var applied = this.applied.understand(ub);
 		var lcl = this.lcl;
-		return nul.possibles.map({app: applied}, function() {
+		return nul.possibles.map(ub.klg, {app: applied}, function() {
 			return new nul.obj.attribute(this.app, lcl);
 		});
 	}
@@ -138,7 +138,7 @@ nul.understanding.base = Class.create({
 	initialize: function(prntUb) {
 		this.parms = {};
 		this.prntUb = prntUb;
-		this.klg = new nul.knowledge(prntUb.klg);
+		this.klg = new nul.knowledge(prntUb?prntUb.klg:null);
 	},
 	resolve: function(identifier) {
 		if(this.prntUb) return this.prntUb.resolve(identifier);
