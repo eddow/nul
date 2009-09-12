b3fcc4e44a4cf5ec74132d43fff0d5ce9fd2b38b
eeddow <eeddow@556927e6-f9aa-11dd-9d1a-b59b2e1864b6>
2009-09-12 09:16:36 +0000

    Big spring refactoring - progression

---
+++ trunk/lng/xpr/obj/defined/nul.obj.litteral.js
@@ -0,0 +1,36 @@
+/*  NUL language JavaScript framework
+ *  (c) 2009 E-med Ware
+ *
+ * NUL is freely distributable under the terms of GNU GPLv3 license.
+ *  For details, see the NUL project site : http://code.google.com/p/nul/
+ *
+ *--------------------------------------------------------------------------*/
+
+nul.obj.litteral = Class.create(nul.obj.defined, {
+	initialise: function(val) {
+		this.value = val;
+		this.type = typeof(val);
+		this.attr = nul.obj.litteral.attr[typeof(val)] 
+	},
+	unify: function(o) { return o.type == this.type && o.value == this.value; }, 
+	ndx: function() { return '['+this.type+':'+(''+this.value).replace(']','[|]')+']'; },
+});
+
+nul.obj.litteral.straightArythmetics = function(oprtr, srnd) {
+	srnd = srnd || '';
+	return function(op1, op2, klg) {
+		if('number'== op2.type) 
+			return new nul.possibles(klg, [nul.obj.number(eval(
+				srnd + op1.value + oprtr + op2.value + srnd
+			))]);
+		if(op2.attr) return [];
+	}
+};
+
+nul.obj.litteral.attr.string = {}
+nul.obj.litteral.attr.number = {};
+
+nul.obj.litteral.attr.string['+'] = nul.obj.litteral.straightArythmetics('"+"','"');
+//TODO4: integers and & | ^
+map(['+', '-', '*', '/', '%'],
+	function(i,v) { nul.obj.litteral.attr.number[v] = nul.obj.litteral.straightArythmetics(v); });
---
+++ trunk/lng/xpr/obj/nul.obj.js
@@ -0,0 +1,47 @@
+/*  NUL language JavaScript framework
+ *  (c) 2009 E-med Ware
+ *
+ * NUL is freely distributable under the terms of GNU GPLv3 license.
+ *  For details, see the NUL project site : http://code.google.com/p/nul/
+ *
+ *--------------------------------------------------------------------------*/
+
+//TODO4: pre-calculate ndx()
+nul.obj = Class.create(nul.xpr, {
+	through: function(o) {
+		//TODO2: return o[this]
+	},
+});
+
+nul.obj.defined = Class.create(nul.obj, {
+	valAttr: function(klg, anm) {
+		var avl = this.attr[anm];
+		if(!avl) return;
+		if('function'!= typeof(avl)) return new nul.possibles(klg, avl);
+		return avl(this, klg);
+	},
+	fctAttr: function(klg, anm, op) {
+		var avl = this.attr[anm];
+		if(!avl) return;
+		if('function'!= typeof(avl)) return nul.possibles.map(klg, {fct: avl}, function(klg) {
+			return op.through(this.fct);
+		});
+		return avl(this, op, klg);
+	},
+	//Default functions through attributes
+	has: function(o, klg) {
+		if(this.attr[' ']) return this.fctAttr(' ', o);
+	},
+});
+
+nul.obj.byAttr = Class.create(nul.obj.defined, {
+	initialise: function(attr) {
+		this.attr = attr||{};
+		this.ndx = ++nul.obj.byAttr.nbr;
+	},
+
+//////////////// nul.xpr implementation
+
+	type: 'byAttr',
+	ndx: function() { return '[o'+this.byAttrNdx+']'; },
+});
