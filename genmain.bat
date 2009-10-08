7e1fe67e8fdf114e4211c6fc8a3f442377440432
eeddow <eeddow@556927e6-f9aa-11dd-9d1a-b59b2e1864b6>
2009-10-08 11:54:39 +0000

    one-file generation

---
+++ genmain.bat
@@ -0,0 +1,40 @@
+DEL null.js
+
+TYPE lng\krnl\null.std.js >> null.js
+TYPE lng\krnl\null.debug.js >> null.js
+TYPE lng\krnl\null.exception.js >> null.js
+TYPE lng\krnl\null.execution.js >> null.js
+TYPE lng\krnl\null.helper.js >> null.js
+TYPE lng\krnl\null.dependance.js >> null.js
+
+TYPE lng\txt\in\null.understand.js >> null.js
+TYPE lng\txt\in\null.compile.js >> null.js
+TYPE lng\txt\in\null.tokenizer.js >> null.js
+
+TYPE lng\txt\out\null.txt.js >> null.js
+TYPE lng\txt\out\null.txt.flat.js >> null.js
+TYPE lng\txt\out\null.txt.html.js >> null.js
+
+TYPE lng\xpr\null.expression.js >> null.js
+
+TYPE lng\algo\null.browse.js >> null.js
+TYPE lng\algo\null.solve.js >> null.js
+
+TYPE lng\xpr\klg\null.xpr.knowledge.js >> null.js
+TYPE lng\xpr\klg\null.xpr.knowledge.eqClass.js >> null.js
+TYPE lng\xpr\klg\null.xpr.knowledge.ior3.js >> null.js
+TYPE lng\xpr\klg\null.xpr.possible.js >> null.js
+
+TYPE lng\xpr\obj\null.xpr.object.js >> null.js
+
+TYPE lng\xpr\obj\defined\null.obj.defined.js >> null.js
+TYPE lng\xpr\obj\defined\null.obj.litteral.js >> null.js
+TYPE lng\xpr\obj\defined\null.obj.pair.js >> null.js
+TYPE lng\xpr\obj\defined\null.obj.sets.js >> null.js
+TYPE lng\xpr\obj\defined\null.obj.lambda.js >> null.js
+TYPE lng\xpr\obj\defined\null.obj.node.js >> null.js
+
+TYPE lng\xpr\obj\undefined\null.obj.undefined.js >> null.js
+TYPE lng\xpr\obj\undefined\null.obj.local.js >> null.js
+TYPE lng\xpr\obj\undefined\null.obj.operation.js >> null.js
+TYPE lng\xpr\obj\undefined\null.obj.ior3.js >> null.js
