1a7beaa648a05a7222b041a25cd604b159171d62
eeddow <eeddow@556927e6-f9aa-11dd-9d1a-b59b2e1864b6>
2009-11-12 23:35:26 +0000

    order some page evol

---
+++ lng/xpr/obj/undefnd/null.obj.undefnd.js
@@ -0,0 +1,19 @@
+/*  NUL language JavaScript framework
+ *  (c) 2009 E-med Ware
+ *
+ * NUL is freely distributable under the terms of GNU GPLv3 license.
+ *  For details, see the NUL project site : http://code.google.com/p/nul/
+ *
+ *--------------------------------------------------------------------------*/
+
+nul.obj.undefnd = Class.create(nul.xpr.object, /** @lends nul.obj.undefnd# */{
+	/**
+	 * Undefined object
+	 * @extends nul.xpr.object
+	 * @constructs
+	 */
+	initialize: function($super) {
+		$super();
+	},
+	defined: false
+});
